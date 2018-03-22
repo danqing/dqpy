a1811d503c39bcfc9d173acdd581fe35b59de37f
Danqing Liu <git@danqing.io>
2018-03-22 15:41:51 +0800

    Set up documentation base

--- README.md
+++
@@ -1,3 +0,0 @@
-# dqpy &middot; ![Travis-CI Status](https://travis-ci.org/danqing/dqpy.svg?branch=master)
-
-Danqing's shared Python library.
--- tests/entities.py
+++
@@ -1,8 +0,0 @@
-from schematics.types import StringType
-
-from dq.entity import Entity
-
-
-class SimpleEntity(Entity):
-
-    name = StringType()
