f6f40f6c1458b48419ca558e3170942e1d8389ef
Danqing Liu <git@danqing.io>
2018-06-03 02:30:53 -0400

    Add github info for docs

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,12 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'danqing',
+    'github_repo': 'dqpy',
+    'github_button': False,
+    'github_banner': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
