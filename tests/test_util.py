f0245b65331671bae4e0a34f6d6fcede6c0ed731
Danqing Liu <git@danqing.io>
2018-06-24 01:20:40 +0800

    Refactor file system related utils into fsutil (#10)

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,60 +1,10 @@
-import os
 import unittest
-from contextlib import suppress
 
 from dq import util
 
 
 class TestUtil(unittest.TestCase):
 
-    def tearDown(self):
-        with suppress(Exception):
-            util.rmrf('test-files')
-
     def test_safe_cast(self):
         assert util.safe_cast('1', int) == 1
         assert util.safe_cast('meow', int, 2) == 2
-
-    def test_safe_mkdirp(self):
-        util.mkdirp('test-files/1/2/3')
-        assert os.path.isdir('test-files/1/2/3')
-
-    def test_safe_rmrf(self):
-        util.mkdirp('test-files/1/2')
-        with open('test-files/1/2/3.txt', 'w') as f:
-            f.write('hello')
-        assert os.path.exists('test-files/1/2/3.txt')
-        util.rmrf('test-files/1/2/3.txt')
-        assert not os.path.exists('test-files/1/2/3.txt')
-        with open('test-files/1/2/3.txt', 'w') as f:
-            f.write('hello')
-        util.rmrf('test-files')
-        assert not os.path.exists('test-files')
-        util.rmrf('none')
-
-    def test_traverse_error(self):
-        self.assertRaises(FileNotFoundError, util.traverse, 'none', None)
-        self.assertRaises(NotADirectoryError, util.traverse, 'README.md', None)
-
-    def test_traverse(self):
-        util.mkdirp('test-files/1/2')
-        with open('test-files/1/2/3.txt', 'w') as f:
-            f.write('hello')
-
-        tree = {}
-
-        def callback(path, isdir):
-            assert path not in tree.keys()
-            tree[path] = isdir
-
-        util.traverse('test-files', callback)
-        assert tree == {'1': True, '1/2': True, '1/2/3.txt': False}
-
-    def test_fileinfo(self):
-        util.mkdirp('test-files')
-        with open('test-files/a.txt', 'w') as f:
-            f.write('hello')
-        assert util.fileinfo('test-files/a.txt') == ('text/plain', 5, None)
-
-    def test_safe_fileinfo(self):
-        assert util.safe_fileinfo(None) == (None, 0, None)
