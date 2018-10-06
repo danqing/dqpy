9adca0ccfd65ddfabbfdbbe7c241dcfbf4537e10
Danqing Liu <git@danqing.io>
2018-10-06 20:49:23 +0800

    Update documentation sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -81,6 +81,11 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# Sidebar components.
+html_sidebars = {
+    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+}
+
 
 # -- Options for HTML output ----------------------------------------------
 
