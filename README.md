19a23b8c474007c1c5b100c5ed4118db38e0f9b8
Danqing Liu <git@danqing.io>
2018-12-16 16:01:26 +0800

    Update pypa documentation and bump version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dqpy &middot; [![Build Status](https://travis-ci.org/danqing/dqpy.svg?branch=master)](https://travis-ci.org/danqing/dqpy) [![Coverage Status](https://coveralls.io/repos/github/danqing/dqpy/badge.svg?branch=master)](https://coveralls.io/github/danqing/dqpy?branch=master) [![PyPI version](https://badge.fury.io/py/dqpy.svg)](https://badge.fury.io/py/dqpy)
 
-Danqing's shared Python library. Documentation: https://dqpy.danqing.io.
+Danqing's shared Python library, for [the better Danqing](https://www.danqing.co). Documentation: https://dqpy.danqing.io.
 
 ## Install
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import find_packages, setup
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 requires = [
     'arrow==0.12.1',
@@ -10,15 +10,22 @@ requires = [
     'PyMySQL==0.9.2',
     'redis==3.0.1',
     'schematics==2.1.0',
-    'SQLAlchemy-Utils==0.33.6',
-    'SQLAlchemy==1.2.14',
+    'SQLAlchemy-Utils==0.33.9',
+    'SQLAlchemy==1.2.15',
     'toml==0.10.0',
 ]
+
+DESCRIPTION = """This is Danqing's shared Python library, for [the better Danqing](https://www.danqing.co).
+
+This library provides convenient APIs for dealing with SQLAlchemy, Redis, configurations, logging, and many other things. The full documentation can be found at the [documentation site](https://dqpy.danqing.io).
+"""
 
 setup(
     name='dqpy',
     version=__version__,
     description='Danqing\'s shared Python library',
+    long_description=DESCRIPTION,
+    long_description_content_type='text/markdown',
     author='Danqing Liu',
     author_email='code@danqing.io',
     url='https://github.com/danqing/dqpy',
