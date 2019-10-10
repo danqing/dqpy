06686e77e6e4e91a4eecb8e892cced0f141d0397
Danqing Liu <git@danqing.io>
2019-10-10 13:57:38 +0800

    Update dependencies (#24)

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import find_packages, setup
 
-__version__ = '2.0.4'
+__version__ = '2.0.5'
 
 requires = [
-    'arrow>=0.14.0,<0.15.0',
+    'arrow>=0.15.0,<0.16.0',
     'emails>=0.5.15,<0.6.0',
-    'redis>=3.2.0,<3.3.0',
+    'redis>=3.3.0,<3.4.0',
     'schematics>=2.1.0,<2.2.0',
     'SQLAlchemy-Utils>=0.34.0,<0.35.0',
     'SQLAlchemy>=1.3.0,<1.4.0',
